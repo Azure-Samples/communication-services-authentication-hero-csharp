0da6d4322dbebd78f368a442a5af46a851483b50
ikumarapeli <ikumarapeli@microsoft.com>
2021-12-14 08:29:55 -0800

    TokenApi initial push

---
+++ TokenApi/Controllers/TokenApiController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using Microsoft.Graph;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.Resource;
+namespace TokenApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [RequiredScope("access_as_user")] //This is the scope we gave the AuthService when registering the application.
+    public class TokenController : Controller
+    {
+        public TokenController(GraphServiceClient graphServiceClient, IOptions<MicrosoftGraphOptions> graphOptions)
+        {
+            _graphServiceClient = graphServiceClient;
+        }
+
+        private readonly GraphServiceClient _graphServiceClient;
+
+        // GET: api/values
+        [HttpGet]
+        public string Get()
+        {
+            User user = _graphServiceClient.Me.Request().GetAsync().GetAwaiter().GetResult();
+            return user.UserPrincipalName;
+        }
+    }
+}
