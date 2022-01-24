b7c7eba095ba2e1a3c2ceba20e978a4f8b6d582d
ikumarapeli <ikumarapeli@microsoft.com>
2022-01-24 14:41:28 -0800

    add comment about endpoint renaming

--- MinimalClient/src/acsAuthApiCaller.js
+++ MinimalClient/src/acsAuthApiCaller.js
@@ -9,7 +9,7 @@ export async function GetAcsToken(accessToken) {
         headers: headers
     };
 
-    return await fetch("https://localhost:5001/api/token", options)
+    return await fetch("https://localhost:5001/api/token", options) //Update this to your deployed endpoint if not testing locally
         .then(response => response.json())
         .catch(error => console.log(error));
 
