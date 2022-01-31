eaa0b8dadf296970deea8a4d45ee4ee93b9ec2f4
ikumarapeli <ikumarapeli@microsoft.com>
2022-01-31 09:57:43 -0800

    use npm instead of yarn and add header files

--- MinimalClient/src/index.js
+++ MinimalClient/src/index.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
