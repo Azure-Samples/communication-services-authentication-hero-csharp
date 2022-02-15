04e1679cfa8c9e02aab87f223e34a4defbbbbfec
ikumarapeli <ikumarapeli@microsoft.com>
2022-02-15 11:31:19 -0800

    formatting issues

--- src/Startup.cs
+++ src/Startup.cs
@@ -77,7 +77,6 @@ namespace ACS.Solution.Authentication.Server
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
                 app.UseHttpsRedirection();
-
             }
 
             app.UseRouting();
